07ef59c70fc1ad50ee1a80aaee9c165d77507061
aokiam <aokial@oregonstate.edu>
2025-06-23 12:02:15 -0700

    setup react environment for webapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl bg-slate-600  text-white text-center font-bold underline">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
